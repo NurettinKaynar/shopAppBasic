5d1f68c6bfdb4c1e4dc17b795d6c50eac77f14a8
Nurettin KAYNAR <nurettin.kaynar@ardgrup.com.tr>
2024-02-09 14:48:02 +0300

    geliştirmeye devam

--- src/core/components/SortByBrands/SortByBrandsComponent.tsx
+++ src/core/components/SortByBrands/SortByBrandsComponent.tsx
@@ -1,11 +1,49 @@
-import { InputText } from "primereact/inputtext"
+import React, { useState } from 'react';
+import { InputText } from 'primereact/inputtext';
+import { Checkbox } from 'primereact/checkbox';
 
-const SortByBrandsComponent = () => {
-  return (
-    <div className="card sahdow-2 flex flex-column" >
-        <InputText className="border-none" placeholder="Search"  />
-    </div>
-  )
+interface Props {
+  brands: string[];
+  onCheckboxChange: (selectedBrands: string[]) => void;
 }
 
-export default SortByBrandsComponent
+const SortByBrandsComponent: React.FC<Props> = ({ brands, onCheckboxChange }) => {
+  const [selectedBrands, setSelectedBrands] = useState<string[]>([]);
+
+  const handleCheckboxChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const value = e.target.value;
+    if (e.target.checked) {
+      setSelectedBrands([...selectedBrands, value]);
+    } else {
+      setSelectedBrands(selectedBrands.filter((brand) => brand !== value));
+    }
+    onCheckboxChange(selectedBrands);
+  };
+
+  return (
+    <div className="card shadow-2 flex flex-column p-4">
+      <span className=" p-input-icon-left">
+        <i className="pi pi-search" />
+        <InputText className=" border-none w-full" placeholder="Search" />
+      </span>
+      <div className="flex flex-column gap-2">
+        {brands&&brands.map((item, index) => (
+          <div className="flex align-items-center" key={index}>
+            <Checkbox
+              inputId={`${item}${index}`}
+              name={item}
+              value={item}
+              checked={selectedBrands.includes(item)}
+              onChange={handleCheckboxChange}
+            />
+            <label htmlFor={`${item}${index}`} className="ml-2">
+              {item}
+            </label>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SortByBrandsComponent;
--- src/core/pages/Home/Home.tsx
+++ src/core/pages/Home/Home.tsx
@@ -1,6 +1,4 @@
-import React from 'react'
-import { RadioButton } from 'primereact/radiobutton';
-import { SortByEnum } from '../../enums';
+import SortByBrandsComponent from '../../components/SortByBrands/SortByBrandsComponent';
 import SortByComponent from '../../components/SortByComponent/SortByComponent';
 const Home = () => {
   return (
@@ -9,7 +7,7 @@ const Home = () => {
 <div className="flex w-full gap-3 md:w-9">
     <div className="flex-none flex-column flex gap-4 w-3">
         <SortByComponent/>
-        <SortByComponent/>
+        <SortByBrandsComponent />
     </div>
     <div className="flex-grow-1 flex align-items-center justify-content-center bg-primary font-bold m-2 h-screen px-5 py-3 border-round">PrimeFlex</div>
     <div className="flex-none flex align-items-center justify-content-center bg-primary font-bold m-2 px-5 py-3 border-round">PrimeFlex</div>
